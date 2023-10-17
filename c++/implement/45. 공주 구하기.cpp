949d0eb5c8e9eb14dad90c6e1fa92db3c71c7ff8
0sun-creater <re2482@naver.com>
2023-10-17 19:38:58 +0900

    practice1017

--- c++/implement/45. 공주 구하기.cpp
+++ c++/implement/45. 공주 구하기.cpp
@@ -36,18 +36,13 @@ int main(){
     
     int e = k-1, _pos =1, pos;
     while(circle.size() > 2){
-        for (int i = 1; i < circle.size(); i++){
-            cout << circle[i] << " ";
-        }
-        cout << endl;
-        pos = _pos+e;
+        pos = _pos+e; 
         if(pos == (int)circle.size()){
             pos = pos - circle.size() + 1;
         }
         if (pos > (int)circle.size()){
-            pos = pos - circle.size();
-        }
-        cout << pos << "번째 out"<< endl;
+            pos = pos - circle.size() - 1;
+        } 
         circle.erase(circle.begin() + pos);
         _pos = pos;
 
@@ -56,3 +51,70 @@ int main(){
 }
 
 
+
+/*
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, k, p=0, i, bp=0, cnt=0;
+	scanf("%d %d", &n, &k);
+	vector<int> prince(n+1);
+	while(1){
+		p++;
+		if(p>n) p=1;
+		if(prince[p]==0){
+			cnt++;
+			if(cnt==k){
+				prince[p]=1;
+				cnt=0;
+				bp++;
+			}
+		}
+		if(bp==n-1) break;
+	}
+	for(i=1; i<=n; i++){
+		if(prince[i]==0){
+			printf("%d\n", i);
+			break;
+		}
+	}
+	return 0;
+}
+
+
+
+
+
+
+
+#include<stdio.h>
+#include<vector>
+using namespace std;
+int main(){
+	freopen("input.txt", "rt", stdin);
+	int n, k, pos=0, i, cnt=0;
+	scanf("%d %d", &n, &k);
+	vector<int> prince(n+1);
+	while(1){
+		for(i=1; i<=k; i++){
+			while(1){
+				pos++;
+				if(pos>n) pos=1;
+				if(prince[pos]==0) break;
+			}
+		}
+		prince[pos]=1;
+		cnt++;
+		if(cnt==n-1) break;
+	}
+	for(i=1; i<=n; i++){
+		if(prince[i]==0){
+			printf("%d\n", i);
+			break;
+		}
+	}
+	return 0;
+}*/
